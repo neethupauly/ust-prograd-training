5c46e6304b2a5334a38a68400ec192c88adc7205
Neethu Pauly <neethupauly146620@gmail.com>
2021-11-28 22:48:06 +0530

    adding Day4

---
+++ Day4/Cube.java
@@ -0,0 +1,11 @@
+public class Cube{
+    // note: use primitive data type for the side of the cube
+  private int side=0;
+  public int getSide(){
+    return this.side;
+  }
+  public int setSide(int num){
+    this.side=num;
+    return side;
+  }
+}
