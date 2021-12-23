177db92af53d7c80fbc4476b1ebdbf2dc5c3e09c
Neethu Pauly <neethupauly146620@gmail.com>
2021-12-23 10:19:19 +0530

    Spring program for MVC added

--- Day22/SpringDay4/ABCbank/src/main/java/com/example/bank/ABCbank/Account.java
+++ Day22/SpringDay4/ABCbank/src/main/java/com/example/bank/ABCbank/Account.java
@@ -1,2 +1,15 @@
-package com.example.bank.ABCbank;public class Account {
+package com.example.bank.ABCbank;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Account {
+
+    public  double balance=2000;
+
+    public double getBalance() {
+        return balance;
+    }
+
+
 }
